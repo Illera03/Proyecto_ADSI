7a4396b399925a582bda1bc8b68b7f8fb71d4c61
AnderCorral <ander.corral2002@gmail.com>
2024-10-16 07:22:25 +0200

    omdb

---
+++ src/omdb_api.py
@@ -0,0 +1,31 @@
+import requests
+
+class OMDBManager:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = "http://www.omdbapi.com/"
+
+    def search_movie(self, title):
+        params = {
+            'apikey': self.api_key,
+            't': title
+        }
+        response = requests.get(self.base_url, params=params)
+        
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
+# Ejemplo de uso
+if __name__ == "__main__":
+    omdb = OMDBManager("TU_API_KEY")
+    movie = omdb.search_movie("The Matrix")
+    
+    if movie and movie['Response'] == 'True':
+        print(f"Título: {movie['Title']}")
+        print(f"Año: {movie['Year']}")
+        print(f"Director: {movie['Director']}")
+        print(f"Género: {movie['Genre']}")
+    else:
+        print("No se encontró la película.")
