c08e1b12d3ca74703756d391c7e126b569ff8219
Jorge <jillera10@gmail.com>
2024-12-04 10:04:02 +0100

    da algún error

--- src/CONTROLADOR/general_management.py
+++ src/CONTROLADOR/general_management.py
@@ -1,6 +1,6 @@
 # Description: General management class.
-import user_management
-import db_manager
+import CONTROLADOR.user_management as UserManager
+import CONTROLADOR.db_manager as db_manager
 class GeneralManager:
     def __init__(self):
         self.__general_manager = GeneralManager()
@@ -10,7 +10,7 @@ class GeneralManager:
         return self.__general_manager
     
     def register_user(self, username, password, email):
-        bien = user_management.user_manager().add_user(username, password, email) # Añadir usuario a la lista de usuarios
+        bien = UserManager.user_manager().add_user(username, password, email) # Añadir usuario a la lista de usuarios
         if bien:
             db_manager.db_manager().insert_user(username, password, email) # Insertar usuario en la base de datos
     
