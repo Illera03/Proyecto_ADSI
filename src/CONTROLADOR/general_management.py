a58a4996c5f4e497d4228c7550ef038c2b880933
Jorge <jillera10@gmail.com>
2024-12-11 09:29:20 +0100

    general_management

--- src/CONTROLADOR/general_management.py
+++ src/CONTROLADOR/general_management.py
@@ -1,13 +1,8 @@
 # Description: General management class.
-<<<<<<< HEAD
-import CONTROLADOR.user_management as UserManager
-import CONTROLADOR.db_manager as db_manager
-import CONTROLADOR.alquiler_management as alquilerman
-=======
 from CONTROLADOR.user_management import UserManager
 from CONTROLADOR.db_manager import DbManager
+from CONTROLADOR.alquiler_management import alquilerman
 
->>>>>>> c7f967029faf408622f4dd3ef7ef160c451daa65
 class GeneralManager:
     _instance = None
 
@@ -25,13 +20,6 @@ class GeneralManager:
         user_manager = UserManager()
         bien = user_manager.add_user(username, password, email)
         if bien:
-<<<<<<< HEAD
-            db_manager.db_manager().insert_user(username, password, email) # Insertar usuario en la base de datos
-
-    def rent_movie(self, user_id, movie_id):
-        """Alquilar una película"""
-        return alquilerman.rent_movie(user_id, movie_id)
-=======
             print("Usuario añadido correctamente.")
             # Usar la misma instancia de DbManager
             db_manager_instance = DbManager()
@@ -42,9 +30,6 @@ class GeneralManager:
         else:
             print("Error al agregar usuario.")
 
-
->>>>>>> c7f967029faf408622f4dd3ef7ef160c451daa65
-    
-        
-    
-
+    def rent_movie(self, user_id, movie_id):
+            """Alquilar una película"""
+            return alquilerman.rent_movie(user_id, movie_id)
